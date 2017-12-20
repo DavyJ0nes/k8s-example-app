91f1cb690fde1f94757c3d517c4922108fa8f4c8
DavyJ0nes <davy.jones@cloudreach.com>
2017-12-20 09:36:16 +0000

    [ADD]: Added health endpoint and readiness check

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,13 +1,27 @@
 package handlers
 
 import (
+	"log"
+	"sync/atomic"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
 // Router is the mux Router for the Service
 func Router(buildTime, commit, release string) *mux.Router {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+	go func() {
+		log.Printf("Readyz probe is negative by default...")
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+		log.Printf("Readyz probe is positive.")
+	}()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home(buildTime, commit, release)).Methods("GET")
 	r.HandleFunc("/healthz", healthz).Methods("GET")
+	r.HandleFunc("/readyz", readyz(isReady)).Methods("GET")
 	return r
 }
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	port := flag.String("port", "8080", "port number to use with Web Service")
 	flag.Parse()
 
-	log.Printf("Starting Service...\ncommit: %s, build_time: %s, release: %s", version.Commit, version.BuildTime, version.Release)
+	log.Printf("Starting Service... | {commit: %s, build_time: %s, release: %s}", version.Commit, version.BuildTime, version.Release)
 	router := handlers.Router(version.BuildTime, version.Commit, version.Release)
 	log.Print("The service is ready to go...")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", *port), router))
